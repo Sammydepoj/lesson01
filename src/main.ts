b56de84a154d55202c991d3821012337841c9de2
Samuel Adepoju <samueladepoju86@yahoo.com>
2024-12-06 14:04:53 +0100

    updates

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: ['debug'] });
+  app.enableCors();
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 9000);
 }
 bootstrap();
